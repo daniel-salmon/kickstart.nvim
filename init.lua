9b6533ec04399eac7ba4abe1588a74efc01b691b
daniel salmon <salmond44@gmail.com>
2025-11-10 13:14:53 -0500

    feat: add xml config

--- init.lua
+++ init.lua
@@ -1031,5 +1031,15 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'xml',
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
